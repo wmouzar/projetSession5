be38452c85048a663be266caffe5fe6cd723d579
wmouzar <31526001+wmouzar@users.noreply.github.com>
2019-02-01 15:17:00 -0500

    Create telephone.js

---
+++ models/telephone.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+// definition shema de telephone
+
+const telephoneSchema = mongoose.Schema({
+	nTelephone:{
+		type: String,
+		required: true
+	},
+	typeTelephone: {
+		type: String,
+        required: true
+	}
+	
+	
+	
+	
+});
+
+// pour rendre Telephone accessible de n'importe ou ailleurs
+const Telephone = module.exports = mongoose.model('Telephone', telephoneSchema);
+
+// get Telephone
+module.exports.getTelephones = function(callback, limit){
+	Telephone.find(callback).limit(limit);
+}
+
+// Get Telephone
+module.exports.getTelephoneById = (id, callback) => {
+	Telephone.findById(id, callback);
+}
